ae27b65f3bd73806d0ec0f488ceb63c0763f4ff5
briankasingli <brian.ksaing.li@gmail.com>
2015-06-14 00:05:20 +0000

    removed the title column

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613214036) do
+ActiveRecord::Schema.define(version: 20150613235204) do
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
     t.string "author"
     t.string "content"
   end
